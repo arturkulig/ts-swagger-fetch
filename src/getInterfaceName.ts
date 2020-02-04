f75fe52d4c27e15fa19c06d7aaa879da5b32ab54
Artur Kulig <arturkulig@gmail.com>
2020-02-04 16:46:09 +0100

    restore interface separators blanks

--- src/getInterfaceName.ts
+++ src/getInterfaceName.ts
@@ -1,4 +1,4 @@
 export function getInterfaceName(input: string) {
-  const lettersAndNumbers = input.replace(/[^a-zA-Z0-9]/g, '');
+  const lettersAndNumbers = input.replace(/[^a-zA-Z0-9]/g, '_');
   return `${lettersAndNumbers[0].toUpperCase()}${lettersAndNumbers.slice(1)}`;
 }
